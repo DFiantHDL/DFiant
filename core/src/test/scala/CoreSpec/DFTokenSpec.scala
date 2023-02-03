18a494a79bc6bc429829eab9667c0aa62fde81c1
oronpo <oron.port@cornell.edu>
2023-02-03 10:16:35 -0500

    minimization

--- core/src/test/scala/CoreSpec/DFTokenSpec.scala
+++ core/src/test/scala/CoreSpec/DFTokenSpec.scala
@@ -4,14 +4,4 @@ import munit.*
 import internals.Inlined
 
 class DFTokenSpec extends FunSuite:
-  test("DFTuple Token Construction") {
-    val t1 = (Bits(8), Bits(8)) token (h"11", b"10010110")
-    val tplDef = (Bits(8), Bits(8))
-    val tplValue = (h"11", b"10010110")
-    val t2 = tplDef token (h"11", b"10010110")
-    val t3 = tplDef token tplValue
-    val z = (Bit, Bits(8), (Bit, Boolean)) token (1, h"11", (true, 0))
-//    assert(t1 == t2)
-//    assert(t2 == t3)
-  }
 end DFTokenSpec
