cb356bf94a1e5f6042145508600bd0d87056ec51
oronpo <oron.port@cornell.edu>
2023-02-03 12:57:51 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFOwner.scala
+++
@@ -1,9 +0,0 @@
-package dfhdl.core
-import dfhdl.internals.*
-import dfhdl.compiler.ir
-
-class DFOwner[+T <: ir.DFOwner](val irValue: T | DFError) extends DFMember[T] //AnyVal with
-object DFOwner:
-  extension [T <: ir.DFOwner](owner: T) def asFE: DFOwner[T] = DFOwner(owner)
-
-type DFOwnerAny = DFOwner[ir.DFOwner]
--- core/src/main/scala/dfhdl/core/asFE.scala
+++ core/src/main/scala/dfhdl/core/asFE.scala
@@ -2,7 +2,6 @@ package dfhdl.core
 import dfhdl.internals.*
 import dfhdl.compiler.ir
 
-export DFOwner.asFE
 export DFType.asFE
 
 given canEqualNothingL: CanEqual[Nothing, Any] = CanEqual.derived
