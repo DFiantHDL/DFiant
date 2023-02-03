a904d9997cb8a6a43c87c73169047708f0c403c8
oronpo <oron.port@cornell.edu>
2023-02-03 12:19:16 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFBoolOrBit.scala
+++ core/src/main/scala/dfhdl/core/DFBoolOrBit.scala
@@ -5,11 +5,8 @@ import dfhdl.internals.*
 
 import annotation.{implicitNotFound, targetName}
 
-type BitNum = 0 | 1
-type BitOrBool = BitNum | Boolean
 type DFBoolOrBit = DFType[ir.DFBoolOrBit, NoArgs]
 object DFBoolOrBit:
-  type Data = Option[Boolean]
   type Token = DFToken[DFBoolOrBit]
   given DFBool = DFBool
   given DFBit = DFBit
@@ -19,4 +16,3 @@ type DFBool = DFType[ir.DFBool.type, NoArgs]
 final lazy val DFBool = ir.DFBool.asFE[DFBool]
 type DFBit = DFType[ir.DFBit.type, NoArgs]
 final lazy val DFBit = ir.DFBit.asFE[DFBit]
-given CanEqual[DFBoolOrBit, DFBoolOrBit] = CanEqual.derived
