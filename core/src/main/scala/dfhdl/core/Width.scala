2effb13acd5ed6ba96d327c49981bd5bcd436b03
oronpo <oron.port@cornell.edu>
2023-02-03 12:49:38 -0500

    minimization

--- core/src/main/scala/dfhdl/core/Width.scala
+++ core/src/main/scala/dfhdl/core/Width.scala
@@ -1,9 +1,6 @@
 package dfhdl
 package core
 import internals.*
-import scala.quoted.*
-import compiler.ir
-import annotation.targetName
 
 trait Width[T]:
   type Out <: Int
@@ -12,12 +9,6 @@ object Width:
     type Out = 1
 end Width
 
-extension [T <: DFTypeAny](token: DFToken[T])
-  @targetName("tokenWidth")
-  def width(using w: Width[T]): Inlined[w.Out] =
-    Inlined.forced[w.Out](token.asIR.width)
-
 extension [T](t: T)(using tc: DFType.TC[T])
-  @targetName("tWidth")
   def width(using w: Width[tc.Type]): Inlined[w.Out] =
     Inlined.forced[w.Out](tc(t).asIR.width)
