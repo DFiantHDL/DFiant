310e09303fea27b45a7cfacc81a3259f1b4acdbf
oronpo <oron.port@cornell.edu>
2023-02-03 13:43:54 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFType.scala
+++ core/src/main/scala/dfhdl/core/DFType.scala
@@ -9,12 +9,13 @@ final class DFType[+T](val value: T | DFError) extends AnyVal
 
 extension [T <: ir.DFType](dfType: DFType[T])
   def asIR: T = dfType.value match
-    case dfTypeIR: T @unchecked => dfTypeIR
-    case err: DFError           => throw new DFError
+    case dfTypeIR: T @unchecked =>
+      println("Match OK!")
+      dfTypeIR
+    case err: DFError => throw new DFError
 
 object Timer
 
-type DFBit = DFType[ir.DFBit.type]
 final lazy val DFBit = new DFType[ir.DFBit.type](ir.DFBit)
 
 extension [T](t: T) def width: Unit = DFBit.asIR
