079e4784a99ad820456fc17a4070f50fac90f3af
oronpo <oron.port@cornell.edu>
2023-02-03 11:01:15 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFTuple.scala
+++ core/src/main/scala/dfhdl/core/DFTuple.scala
@@ -150,49 +150,6 @@ object DFTuple:
             zipper(dfType.fieldList, value.toList).map(_.asIR.data)
           )
     end Compare
-
-    object Ops:
-      import CompanionsDFBits.BitIndex
-      extension [T <: NonEmptyTuple](t: DFToken[DFTuple[T]])
-        def apply[I <: Int](i: Inlined[I])(using
-            check: BitIndex.Check[I, Tuple.Size[T]],
-            size: ValueOf[Tuple.Size[T]]
-        ): DFToken[DFType.FromDFVal[Tuple.Elem[T, I]]] =
-          check(i, size)
-          selectRuntime[DFType.FromDFVal[Tuple.Elem[T, I]]](t.wide, i)
-      private def selectRuntime[T <: DFTypeAny](
-          token: Token[NonEmptyTuple],
-          idx: Int
-      ): DFToken[T] =
-        val dfType = token.dfType.fieldList(idx).asIR
-        val data = token.data(idx)
-        ir.DFToken.forced(dfType, data).asTokenOf[T]
-      extension [T <: NonEmptyTuple](t: DFToken[DFTuple[T]])
-        // TODO: workaround compiler issue that inline does not obey covariance
-        private def wide: Token[NonEmptyTuple] =
-          t.asInstanceOf[Token[NonEmptyTuple]]
-
-//      extension [T1 <: DFTypeAny](t: Token[Tuple1[DFValOf[T1]]])
-//        inline def _1: DFToken[T1] =
-//          selectRuntime[T1](t.wide, 0)
-//      extension [T1 <: DFTypeAny, T2 <: DFTypeAny](
-//          t: Token[(DFValOf[T1], DFValOf[T2])]
-//      )
-//        inline def _1: DFToken[T1] =
-//          selectRuntime[T1](t.wide, 0)
-//        inline def _2: DFToken[T2] =
-//          selectRuntime[T2](t.wide, 1)
-//      extension [T1 <: DFTypeAny, T2 <: DFTypeAny, T3 <: DFTypeAny](
-//          t: Token[(DFValOf[T1], DFValOf[T2], DFValOf[T3])]
-//      )
-//        inline def _1: DFToken[T1] =
-//          selectRuntime[T1](t.wide, 0)
-//        inline def _2: DFToken[T2] =
-//          selectRuntime[T2](t.wide, 1)
-//        inline def _3: DFToken[T3] =
-//          selectRuntime[T3](t.wide, 2)
-    end Ops
-
   end Token
 
   object Val:
@@ -239,48 +196,5 @@ object DFTuple:
           DFVal.Func(dfType, FuncOp.++, dfVals)(using dfc.anonymize)
     end Compare
 
-    object Ops:
-      import CompanionsDFBits.BitIndex
-      extension [T <: NonEmptyTuple, M <: ModifierAny](t: DFVal[DFTuple[T], M])
-        def apply[I <: Int](i: Inlined[I])(using
-            dfc: DFC,
-            check: BitIndex.Check[I, Tuple.Size[T]],
-            size: ValueOf[Tuple.Size[T]]
-        ): DFVal[DFType.FromDFVal[Tuple.Elem[T, I]], M] = trydf {
-          check(i, size)
-          applyForced[DFType.FromDFVal[Tuple.Elem[T, I]]](i)
-        }
-        private[core] def applyForced[OT <: DFTypeAny](i: Int)(using
-            dfc: DFC
-        ): DFVal[OT, M] = DFVal.Alias
-          .SelectField(t, s"_${i + 1}")
-          .asIR
-          .asVal[OT, M]
-      end extension
-//      extension [T1 <: DFTypeAny, M <: ModifierAny](
-//          t: DFVal[DFTuple[Tuple1[DFValOf[T1]]], M]
-//      )
-//        inline def _1(using DFC): DFVal[T1, M] =
-//          t.applyForced[T1](0)
-//      extension [T1 <: DFTypeAny, T2 <: DFTypeAny, M <: ModifierAny](
-//          t: DFVal[DFTuple[(DFValOf[T1], DFValOf[T2])], M]
-//      )
-//        inline def _1(using DFC): DFVal[T1, M] =
-//          t.applyForced[T1](0)
-//        inline def _2(using DFC): DFVal[T2, M] =
-//          t.applyForced[T2](1)
-//      extension [
-//          T1 <: DFTypeAny,
-//          T2 <: DFTypeAny,
-//          T3 <: DFTypeAny,
-//          M <: ModifierAny
-//      ](t: DFVal[DFTuple[(DFValOf[T1], DFValOf[T2], DFValOf[T3])], M])
-//        inline def _1(using DFC): DFVal[T1, M] =
-//          t.applyForced[T1](0)
-//        inline def _2(using DFC): DFVal[T2, M] =
-//          t.applyForced[T2](1)
-//        inline def _3(using DFC): DFVal[T3, M] =
-//          t.applyForced[T3](2)
-    end Ops
   end Val
 end DFTuple
