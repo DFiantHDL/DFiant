86995281048afbfc7916f03830b80c6076501111
oronpo <oron.port@cornell.edu>
2023-02-03 10:38:08 -0500

    minimization

--- core/src/main/scala/dfhdl/hdl.scala
+++ core/src/main/scala/dfhdl/hdl.scala
@@ -1,17 +1,11 @@
 package dfhdl
 object hdl:
   class dsn extends scala.annotation.StaticAnnotation
-  export core.DFType.Ops.*
-  export core.DFToken.Ops.*
   export core.DFBoolOrBit.Token.Ops.*
   export core.DFBoolOrBit.Val.Ops.*
-  export core.{width, dfType}
-  export core.Timer.Ops.*
-  export core.Wait.Ops.*
+  export core.width
   type Timer = core.Timer
   val Timer = core.Timer
-  export core.dfc
-  export internals.Inlined
 
   lazy val Bit = core.DFBit
   type Bit = core.DFBit
