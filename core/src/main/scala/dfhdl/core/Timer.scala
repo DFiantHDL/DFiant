dd2728f5887917da6cfefc6f28a60fb0e39b435e
oronpo <oron.port@cornell.edu>
2023-02-03 12:43:23 -0500

    minimization

--- core/src/main/scala/dfhdl/core/Timer.scala
+++ core/src/main/scala/dfhdl/core/Timer.scala
@@ -1,15 +1,3 @@
 package dfhdl.core
-import dfhdl.compiler.ir
-import ir.{Time, Freq, Ratio}
-import ir.Timer.Func.Op as FuncOp
-import dfhdl.internals.*
 
-sealed class Timer private (val irValue: ir.Timer | DFError) extends DFMember[ir.Timer]
-object Timer:
-  extension (timer: ir.Timer) private def asFE: Timer = new Timer(timer)
-  type Period = Time | Freq
-  extension (period: Period)
-    def time = period match
-      case t: Time => t
-      case f: Freq => f.period
-end Timer
+object Timer
