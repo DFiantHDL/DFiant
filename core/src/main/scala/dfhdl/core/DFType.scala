b45c66450c5e11d37efa660bd858fe0accac2561
oronpo <oron.port@cornell.edu>
2023-02-03 12:56:54 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFType.scala
+++ core/src/main/scala/dfhdl/core/DFType.scala
@@ -16,20 +16,8 @@ sealed trait Args3[T1, T2, T3] extends Args
 final class DFType[+T <: ir.DFType, +A <: Args](val value: T | DFError) extends AnyVal:
   override def toString: String = value.toString
 type DFTypeAny = DFType[ir.DFType, Args]
-val NoType = new DFType[ir.NoType.type, NoArgs](ir.NoType)
 
 object DFType:
-  type Of[T <: Supported] <: DFTypeAny = T match
-    case DFTypeAny => T <:! DFTypeAny
-    case Boolean   => DFBool
-
-  def of[T <: Supported](t: T): Of[T] = DFType(t).asInstanceOf[Of[T]]
-  private[core] def apply(t: Any): DFTypeAny =
-    t match
-      case dfType: DFTypeAny => dfType
-      case _: Byte.type      => DFBits(8)
-      case _: Boolean.type   => DFBool
-  private[core] def unapply(t: Any): Option[DFTypeAny] = ???
 
   extension [T <: ir.DFType, A <: Args](dfType: DFType[T, A])
     def asIR: T = dfType.value match
@@ -37,8 +25,6 @@ object DFType:
       case err: DFError           => throw DFError.Derived(err)
   extension (dfType: ir.DFType) def asFE[T <: DFTypeAny]: T = new DFType(dfType).asInstanceOf[T]
   export DFBoolOrBit.given
-
-  type Supported = DFTypeAny | Byte | Long | Boolean | AnyRef
 
   trait TC[T]:
     type Type <: DFTypeAny
