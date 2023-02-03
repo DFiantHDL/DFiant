17891402cee058c34f85441678a07d8cf770439e
oronpo <oron.port@cornell.edu>
2023-02-03 13:36:14 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFType.scala
+++ core/src/main/scala/dfhdl/core/DFType.scala
@@ -11,18 +11,14 @@ final class DFType[+T](val value: T | DFError) extends AnyVal:
   override def toString: String = value.toString
 type DFTypeAny = DFType[ir.DFType]
 
-object DFType:
-  extension [T <: ir.DFType](dfType: DFType[T])
-    def asIR: T = dfType.value match
-      case dfTypeIR: T @unchecked => dfTypeIR
-      case err: DFError           => throw new DFError
-  extension (dfType: ir.DFType) def asFE[T <: DFTypeAny]: T = new DFType(dfType).asInstanceOf[T]
-end DFType
+extension [T <: ir.DFType](dfType: DFType[T])
+  def asIR: T = dfType.value match
+    case dfTypeIR: T @unchecked => dfTypeIR
+    case err: DFError           => throw new DFError
 
-export DFType.asFE
 object Timer
 
 type DFBit = DFType[ir.DFBit.type]
-final lazy val DFBit = ir.DFBit.asFE[DFBit]
+final lazy val DFBit = new DFType(ir.DFBit).asInstanceOf[DFBit]
 
 extension [T](t: T) def width: Unit = DFBit.asIR
