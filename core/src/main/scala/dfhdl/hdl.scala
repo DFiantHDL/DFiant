31eab42d50f5ce14b4196f16acf685ae4a13f90e
oronpo <oron.port@cornell.edu>
2023-02-03 10:35:40 -0500

    minimization

--- core/src/main/scala/dfhdl/hdl.scala
+++ core/src/main/scala/dfhdl/hdl.scala
@@ -5,24 +5,24 @@ object hdl:
   export core.DFToken.Ops.*
   export core.DFBoolOrBit.Token.Ops.*
   export core.DFBoolOrBit.Val.Ops.*
-  export core.DFBits.Token.Ops.*
-  export core.DFBits.Token.StrInterp.{h, b}
-  export core.DFBits.Val.Ops.*
-  export core.DFBits.Val.TupleOps.*
-  export core.DFDecimal.Token.StrInterp.{d, sd}
-  export core.DFDecimal.Token.Ops.*
-  export core.DFDecimal.Val.Ops.*
-  export core.DFVector.Token.Ops.*
-  export core.DFVector.Val.Ops.*
-  export core.DFOpaque.Token.Ops.*
-  export core.DFOpaque.Val.Ops.*
-  export core.DFTuple.Token.Ops.*
-  export core.DFTuple.Val.Ops.*
-  export core.DFVector.Ops.*
-  export core.DFVal.Ops.*
-  export core.DFVarOps.*
-  export core.DFPortOps.*
-  export internals.CommonOps.*
+//  export core.DFBits.Token.Ops.*
+//  export core.DFBits.Token.StrInterp.{h, b}
+//  export core.DFBits.Val.Ops.*
+//  export core.DFBits.Val.TupleOps.*
+//  export core.DFDecimal.Token.StrInterp.{d, sd}
+//  export core.DFDecimal.Token.Ops.*
+//  export core.DFDecimal.Val.Ops.*
+//  export core.DFVector.Token.Ops.*
+//  export core.DFVector.Val.Ops.*
+//  export core.DFOpaque.Token.Ops.*
+//  export core.DFOpaque.Val.Ops.*
+//  export core.DFTuple.Token.Ops.*
+//  export core.DFTuple.Val.Ops.*
+//  export core.DFVector.Ops.*
+//  export core.DFVal.Ops.*
+//  export core.DFVarOps.*
+//  export core.DFPortOps.*
+//  export internals.CommonOps.*
   export core.{width, dfType}
   export core.Timer.Ops.*
   export core.Wait.Ops.*
@@ -44,59 +44,59 @@ object hdl:
   val SInt = core.DFSInt
   type Encode = core.DFEncoding.Default
   val Encode = core.DFEncoding
-  export core.DFStruct.Fields as Struct
-  export core.DFOpaque.Frontend as Opaque
-  type DFDesign = core.DFDesign
-  type RTDesign = core.RTDesign
-  type EDDesign = core.EDDesign
-  type EDBlackBox = core.EDBlackBox
-  val EDBlackBox = core.EDBlackBox
-  type DFDomain = core.DFDomain
-  type RTDomain = core.RTDomain
-  type EDDomain = core.EDDomain
+//  export core.DFStruct.Fields as Struct
+//  export core.DFOpaque.Frontend as Opaque
+//  type DFDesign = core.DFDesign
+//  type RTDesign = core.RTDesign
+//  type EDDesign = core.EDDesign
+//  type EDBlackBox = core.EDBlackBox
+//  val EDBlackBox = core.EDBlackBox
+//  type DFDomain = core.DFDomain
+//  type RTDomain = core.RTDomain
+//  type EDDomain = core.EDDomain
 
-  val IN = core.Modifier.IN
-  val OUT = core.Modifier.OUT
-  val INOUT = core.Modifier.INOUT
-  val VAR = core.Modifier.VAR
-  val WIRE = core.Modifier.WIRE
-  export core.Modifier.REG
-  type VAL = core.VAL
-  type TOKEN = core.TOKEN
-  export core.<>
-  export core.X
-  export core.Process.Ops.*
-
-  val DerivedCfg = core.DerivedCfg
-  type ClkCfg = core.ClkCfg
-  val ClkCfg = core.ClkCfg
-  type RstCfg = core.RstCfg
-  val RstCfg = core.RstCfg
-  type RTDomainCfg = core.RTDomainCfg
-  val RTDomainCfg = core.RTDomainCfg
-  val NoClockCfg = RTDomainCfg(None, None)
-
-  // shorthand for annotating a DFBits value (useful for string interpolation)
-  type B[W <: Int] = core.DFValOf[Bits[W]]
-  val ? = core.?
-  export core.SameElementsVector as all
-  extension [Entry <: core.DFEncoding](e: Entry)
-    def unapply[E >: Entry <: core.DFEncoding](arg: core.DFValOf[core.DFEnum[E]])(using
-        DFC
-    ): Boolean =
-      ???
-
-  inline implicit def __refined_token[T <: core.FieldsOrTuple](
-      token: core.DFToken[core.DFStruct[T]]
-  )(using
-      r: core.DFToken.Refiner[T]
-  ): r.Out = token.asInstanceOf[r.Out]
-
-  inline implicit def __refined_dfVal[T <: core.FieldsOrTuple, A, I](
-      dfVal: core.DFVal[core.DFStruct[T], core.Modifier[A, Any, I]]
-  )(using
-      r: core.DFVal.Refiner[T, A, I]
-  ): r.Out = dfVal.asInstanceOf[r.Out]
+//  val IN = core.Modifier.IN
+//  val OUT = core.Modifier.OUT
+//  val INOUT = core.Modifier.INOUT
+//  val VAR = core.Modifier.VAR
+//  val WIRE = core.Modifier.WIRE
+//  export core.Modifier.REG
+//  type VAL = core.VAL
+//  type TOKEN = core.TOKEN
+//  export core.<>
+//  export core.X
+//  export core.Process.Ops.*
+//
+//  val DerivedCfg = core.DerivedCfg
+//  type ClkCfg = core.ClkCfg
+//  val ClkCfg = core.ClkCfg
+//  type RstCfg = core.RstCfg
+//  val RstCfg = core.RstCfg
+//  type RTDomainCfg = core.RTDomainCfg
+//  val RTDomainCfg = core.RTDomainCfg
+//  val NoClockCfg = RTDomainCfg(None, None)
+//
+//  // shorthand for annotating a DFBits value (useful for string interpolation)
+//  type B[W <: Int] = core.DFValOf[Bits[W]]
+//  val ? = core.?
+//  export core.SameElementsVector as all
+//  extension [Entry <: core.DFEncoding](e: Entry)
+//    def unapply[E >: Entry <: core.DFEncoding](arg: core.DFValOf[core.DFEnum[E]])(using
+//        DFC
+//    ): Boolean =
+//      ???
+//
+//  inline implicit def __refined_token[T <: core.FieldsOrTuple](
+//      token: core.DFToken[core.DFStruct[T]]
+//  )(using
+//      r: core.DFToken.Refiner[T]
+//  ): r.Out = token.asInstanceOf[r.Out]
+//
+//  inline implicit def __refined_dfVal[T <: core.FieldsOrTuple, A, I](
+//      dfVal: core.DFVal[core.DFStruct[T], core.Modifier[A, Any, I]]
+//  )(using
+//      r: core.DFVal.Refiner[T, A, I]
+//  ): r.Out = dfVal.asInstanceOf[r.Out]
 
 end hdl
 
