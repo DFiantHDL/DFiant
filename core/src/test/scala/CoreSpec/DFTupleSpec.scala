8f7e7d7bc7a98bde1f79e4b6c4cb21587dc41e46
oronpo <oron.port@cornell.edu>
2023-02-03 10:13:57 -0500

    minimization

--- core/src/test/scala/CoreSpec/DFTupleSpec.scala
+++ core/src/test/scala/CoreSpec/DFTupleSpec.scala
@@ -4,7 +4,5 @@ import munit.*
 
 class DFTupleSpec extends DFSpec:
   val tplA = (UInt(8), Bit, Bits(3))
-  val tplB = ((UInt(8), Bit), Bits(3))
   val tokenA = tplA token (22, 1, b"101")
-  val tokenB = tplB token ((22, 1), b"101")
 end DFTupleSpec
