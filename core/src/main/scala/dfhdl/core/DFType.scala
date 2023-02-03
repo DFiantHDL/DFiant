e4a4d5fd2caf733bff23caea4852dc9722945070
oronpo <oron.port@cornell.edu>
2023-02-03 13:30:18 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFType.scala
+++ core/src/main/scala/dfhdl/core/DFType.scala
@@ -41,11 +41,4 @@ end DFBoolOrBit
 type DFBit = DFType[ir.DFBit.type]
 final lazy val DFBit = ir.DFBit.asFE[DFBit]
 
-trait Width[T]:
-  type Out <: Int
-object Width:
-  given fromDFBoolOrBit[T <: DFBoolOrBit]: Width[T] with
-    type Out = 1
-end Width
-
 extension [T](t: T)(using tc: DFType.TC[T]) def width: Unit = tc(t).asIR
