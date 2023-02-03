b31cbdaeee6f00dfbca6549372d86ce9da2a0018
oronpo <oron.port@cornell.edu>
2023-02-03 11:48:25 -0500

    minimization

--- core/src/main/scala/dfhdl/core/DFBits.scala
+++ core/src/main/scala/dfhdl/core/DFBits.scala
@@ -26,46 +26,8 @@ object DFBits:
   ): DFBits[W] = DFBits[W](Inlined.forced[W](valueOf[W]))
 
   type Token[W <: Int] = CompanionsDFBits.Token[W]
-  val Token = CompanionsDFBits.Token
 end DFBits
 
 private object CompanionsDFBits:
   type Token[W <: Int] = DFToken[DFBits[W]]
-  object Token:
-    protected[core] def apply[W <: Int](
-        dfType: DFBits[W],
-        data: (BitVector, BitVector)
-    ): Token[W] =
-      ir.DFToken(dfType.asIR)(data).asTokenOf[DFBits[W]]
-    protected[core] def apply[W <: Int](
-        width: Inlined[W],
-        valueBits: BitVector,
-        bubbleBits: BitVector
-    ): Token[W] =
-      Token(DFBits(width), (valueBits, bubbleBits))
-    protected[core] def apply[W <: Int](
-        width: Inlined[W],
-        value: Bubble
-    ): Token[W] =
-      Token(
-        width,
-        BitVector.low(width.value),
-        BitVector.high(width.value)
-      )
-    protected[core] def apply[W <: Int, T <: BitOrBool](
-        width: Inlined[W],
-        sev: SameElementsVector[T]
-    ): Token[W] =
-      val boolVal = sev.value match
-        case b: Boolean => b
-        case i: Int     => i > 0
-      Token(
-        width,
-        BitVector.fill(width.value)(boolVal),
-        BitVector.low(width.value)
-      )
-    extension [W <: Int](token: DFBits.Token[W])
-      def valueBits: BitVector = token.data._1
-      def bubbleBits: BitVector = token.data._2
-  end Token
 end CompanionsDFBits
