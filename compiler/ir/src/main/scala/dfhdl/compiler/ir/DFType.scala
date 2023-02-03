0c9bcbeb314708cef53344c2d8156a6412833a19
oronpo <oron.port@cornell.edu>
2023-02-03 13:24:33 -0500

    minimization

--- compiler/ir/src/main/scala/dfhdl/compiler/ir/DFType.scala
+++ compiler/ir/src/main/scala/dfhdl/compiler/ir/DFType.scala
@@ -1,19 +1,6 @@
 package dfhdl.compiler
 package ir
-import dfhdl.internals.*
 
-import scala.collection.immutable.{ListMap, ListSet}
-import scala.reflect.ClassTag
-
-sealed trait DFType extends Product, Serializable derives CanEqual
-object DFType
-
-/////////////////////////////////////////////////////////////////////////////
-// DFBool or DFBit
-/////////////////////////////////////////////////////////////////////////////
+sealed trait DFType
 sealed trait DFBoolOrBit extends DFType
-
-object DFBoolOrBit
-
 case object DFBit extends DFBoolOrBit
-/////////////////////////////////////////////////////////////////////////////
